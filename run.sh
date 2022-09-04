78f1ad9402f98644e64330f367826d4fe40b76ae
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-09-04 09:57:23 -0700

    run all four models to completion

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ source ~/miniforge3/etc/profile.d/conda.sh # Local
 conda activate rectangles
 
 # dev
-time ./scripts/run_full_experiment.sh configs/dev.yml
+# time ./scripts/run_full_experiment.sh configs/dev.yml
 
 # run multiple architectures
 
-# time ./scripts/run_full_experiment.sh configs/mlp0.yml
-# time ./scripts/run_full_experiment.sh configs/mlp1.yml
-# time ./scripts/run_full_experiment.sh configs/cnn0.yml
-# time ./scripts/run_full_experiment.sh configs/cnn1.yml
+time ./scripts/run_full_experiment.sh configs/mlp0.yml
+time ./scripts/run_full_experiment.sh configs/mlp1.yml
+time ./scripts/run_full_experiment.sh configs/cnn0.yml
+time ./scripts/run_full_experiment.sh configs/cnn1.yml
