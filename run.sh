e960e0f8397e34c5ac14802dbcead307e89df790
Nathaniel <nimel@gmail.com>
2022-09-04 22:12:08 -0700

    mlp does not learn many categories in 50 epochs

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source ~/anaconda3/etc/profile.d/conda.sh # Patas
 conda activate rectangles
 
 # dev
-time ./scripts/run_full_experiment.sh configs/dev.yml
+# time ./scripts/run_full_experiment.sh configs/dev.yml
 
 # run multiple architectures
 
@@ -12,3 +12,5 @@ time ./scripts/run_full_experiment.sh configs/dev.yml
 # time ./scripts/run_full_experiment.sh configs/mlp1.yml
 # time ./scripts/run_full_experiment.sh configs/cnn0.yml
 # time ./scripts/run_full_experiment.sh configs/cnn1.yml
+
+time ./scripts/run_full_experiment.sh configs/mlp2.yml
